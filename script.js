c330a144cef96234f9bfec4664b9ca582e778850
Marcell Hayes <marcell.made@gmail.com>
2017-04-23 09:23:06 -0400

    found equation for area and able to convert em to cm. Still trying to figure out how to append string with variable. Works fine as alert.

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ document.getElementById("confirm").onclick = function calculate() {
     var fat = document.getElementById("fatness"), 
         ball = document.getElementById("cat-ball"),
         fatAmmount = fat.value,
-        weight = fatAmmount / 5;
+        weight = fatAmmount / 1.2,
+        radius= weight / 2,
+        areaEm = (radius * 3.14) * 2,
+        areaCm = areaEm / 2.371;
     
-    alert(weight);
+    alert("Your cat is " + areaCm + "cm big!");
+    
+//    document.getElementsByTagName("P")[0].insertAdjacentHTML("Your cat is ", areaCm, "cm big!");
+            
     
 // whenever I change this from .innerHTML it says that the function is undefined. Do I need parameters?
     document.getElementsByTagName("P")[0].innerHTML = "Hi";
