7578953db87f6e69f3e0829bf1008bf346624482
Marcell Hayes <marcell.made@gmail.com>
2017-04-22 22:33:29 -0400

    initial push. circle added. variable for slider value corrently referenced. still getting circle to be responsive. still working out calculation for area.

---
+++ script.js
@@ -0,0 +1,40 @@
+//Function runs as button is pushed
+function calculate() {
+        
+    var fat = document.getElementById("fatness"), 
+        ball = document.getElementById("cat-ball"),
+        fatAmmount = fat.value,
+        width = fatAmmount / 10,
+        msg = document.getElementById("p"),
+        weight = findWeight(fat.value);
+    
+
+    alert(fatAmmount);    
+
+        
+    
+        
+
+    
+     function findWeight(r, width) {
+            var area = (r * 3.14) * 2;
+            r = width / 2;
+        }
+    
+    console.log("Your cat is " + weight + " big!"); 
+    };
+
+    document.p.appendChild("Your cat is " + weight + " big!");
+
+
+/*Initially my findWeight funciton was inside my sentece function. However, this way, the console log would never be called, so I separated them. That way the console log when be called when the button is clicked like everything else in calculate()*/
+
+//    var sentence = function(weight) {       
+//        }
+    
+    
+    
+//    $('input[name=fatSlider]').val();
+//    
+//    $('input[name=fatSlider]').on('change', handleChange);
+
