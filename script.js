2b9089698340947b5bed56f3a2451a78d483a94a
Marcell Hayes <marcell.made@gmail.com>
2017-04-23 09:26:23 -0400

    got it. Maybe I can find something to use besides innerHTML later.

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ document.getElementById("confirm").onclick = function calculate() {
         areaEm = (radius * 3.14) * 2,
         areaCm = areaEm / 2.371;
     
-    alert("Your cat is " + areaCm + "cm big!");
+//    alert("Your cat is " + areaCm + "cm big!");
     
 //    document.getElementsByTagName("P")[0].insertAdjacentHTML("Your cat is ", areaCm, "cm big!");
             
     
-// whenever I change this from .innerHTML it says that the function is undefined. Do I need parameters?
-    document.getElementsByTagName("P")[0].innerHTML = "Hi";
+// Dunno if innerText will work since it has a variable.
+    document.getElementsByTagName("P")[0].innerHTML = "Your cat is " + areaCm + "cm big!";
     
     ball.style.width = weight + "em";
     ball.style.height = weight + "em";
