ad7708344d768c3eddd58c4f2ed28659fb577970
Marcell Hayes <marcell.made@gmail.com>
2017-04-22 23:03:20 -0400

    after removing variables from findWeight, console kept saying function was undefined when trying to append message to p element. Solved this by making an EventListener outside the function.

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
+ document.getElementById("confirm").addEventListener("click", calculate);
+
 //Function runs as button is pushed
 function calculate() {
         
     var fat = document.getElementById("fatness"), 
         ball = document.getElementById("cat-ball"),
         fatAmmount = fat.value,
-        width = fatAmmount / 10,
-        msg = document.getElementById("p"),
-        weight = findWeight(fat.value);
-    
-
-    alert(fatAmmount);    
-
+        width = fatAmmount / 10;
+//        msg = document.getElementsByTagName("P"),
+//        weight = findWeight(fat.value);
         
     
+
+//    alert(fatAmmount);    
+//    msg.appendChild("Hi");
+        
+    document.getElementsByTagName("P")[0].innerHTML = "Hi";
         
 
     
-     function findWeight(r, width) {
-            var area = (r * 3.14) * 2;
-            r = width / 2;
-        }
-    
-    console.log("Your cat is " + weight + " big!"); 
+//     function findWeight(r, width) {
+//            var area = (r * 3.14) * 2;
+//            r = width / 2;
+//        }
+//    
+//    console.log("Your cat is " + weight + " big!"); 
     };
 
-    document.p.appendChild("Your cat is " + weight + " big!");
+  
 
 
 /*Initially my findWeight funciton was inside my sentece function. However, this way, the console log would never be called, so I separated them. That way the console log when be called when the button is clicked like everything else in calculate()*/
